0dd612b41cba39aadb5b58c4930f8aae958177d1
Georgy Moshkin <gmoshkin@picodata.io>
2024-08-02 21:26:00 +0300

    to_ascii_or_hex_simd is slow :(

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,15 @@
 use core::arch::x86_64::*;
 use std::mem::*;
 
-pub const ZERO:      __m256i = unsafe { transmute([0b00000000_u8; 32]) };
-pub const TOP_BIT:   __m256i = unsafe { transmute([0b10000000_u8; 32]) };
-pub const BITS_23:   __m256i = unsafe { transmute([0b01100000_u8; 32]) };
-pub const ASCII_END: __m256i = unsafe { transmute([0b01111111_u8; 32]) };
+pub const ASCII_END:    __m256i = unsafe { transmute([b'~' + 1; 32]) };
+pub const ASCII_START:  __m256i = unsafe { transmute([b' ' - 1; 32]) };
+pub const BACKSLASH:    __m256i = unsafe { transmute([b'\\'; 32]) };
+pub const DOUBLE_QUOTE: __m256i = unsafe { transmute([b'"'; 32]) };
 
 const VECTOR_SIZE: usize = std::mem::size_of::<__m256i>();
 
 #[no_mangle]
-#[inline(never)]
+// #[inline(never)]
 pub fn find_ascii_simd(data: &[u8]) -> usize {
     let range = data.as_ptr_range();
     let mut p = range.start;
@@ -19,27 +19,13 @@ pub fn find_ascii_simd(data: &[u8]) -> usize {
         while p.add(VECTOR_SIZE) < range.end {
             let v = _mm256_loadu_si256(p as _);
 
-            let step1 = _mm256_andnot_si256(v, TOP_BIT);
-            let step1 = _mm256_cmpgt_epi8(ZERO, step1);
-
-            let step2 = _mm256_and_si256(v, BITS_23);
-            let step2 = _mm256_cmpgt_epi8(step2, ZERO);
-
-            let step3 = _mm256_cmpeq_epi8(v, ASCII_END);
-            let step3 = _mm256_cmpeq_epi8(step3, ZERO);
-
-            let step1_2 = _mm256_and_si256(step1, step2);
-
-            let mask = _mm256_and_si256(step1_2, step3);
-            // inspect!(mask);
-            let mask = _mm256_movemask_epi8(mask);
-            // inspect!(mask);
+            let lower_bound = _mm256_cmpgt_epi8(v, ASCII_START);
+            let upper_bound = _mm256_cmpgt_epi8(ASCII_END, v);
+            let in_bounds = _mm256_and_si256(lower_bound, upper_bound);
+            let mask = _mm256_movemask_epi8(in_bounds);
             let offset = mask.trailing_zeros();
-            // println!("leading zeros: {offset}");
-            // println!("trailing zeros: {}", mask.trailing_zeros());
 
             if mask != 0 {
-                // dbg!((p.offset_from(range.start), offset));
                 return p.add(offset as _).offset_from(range.start) as _;
             }
 
@@ -60,13 +46,29 @@ pub fn find_ascii_simd(data: &[u8]) -> usize {
 
 #[no_mangle]
 #[inline(never)]
-pub fn find_ascii_non_simd(data: &[u8]) -> usize {
+pub fn find_backslash_or_double_quote_simd(data: &[u8]) -> usize {
     let range = data.as_ptr_range();
     let mut p = range.start;
 
     unsafe {
+        while p.add(VECTOR_SIZE) < range.end {
+            let v = _mm256_loadu_si256(p as _);
+
+            let backslash = _mm256_cmpeq_epi8(v, BACKSLASH);
+            let double_quote = _mm256_cmpeq_epi8(v, DOUBLE_QUOTE);
+            let either = _mm256_or_si256(backslash, double_quote);
+            let mask = _mm256_movemask_epi8(either);
+            let offset = mask.trailing_zeros();
+
+            if mask != 0 {
+                return p.add(offset as _).offset_from(range.start) as _;
+            }
+
+            p = p.add(VECTOR_SIZE);
+        }
+
         while p < range.end {
-            if *p >= b' ' && *p <= b'~' {
+            if *p == b'\\' || *p == b'"' {
                 return p.offset_from(range.start) as _;
             }
 
@@ -75,6 +77,179 @@ pub fn find_ascii_non_simd(data: &[u8]) -> usize {
     }
 
     data.len()
+}
+
+#[no_mangle]
+// #[inline(never)]
+pub fn find_non_ascii_simd(data: &[u8]) -> usize {
+    let range = data.as_ptr_range();
+    let mut p = range.start;
+
+    unsafe {
+        while p.add(VECTOR_SIZE) < range.end {
+            let v = _mm256_loadu_si256(p as _);
+
+            let lower_bound = _mm256_cmpgt_epi8(v, ASCII_START);
+            let upper_bound = _mm256_cmpgt_epi8(ASCII_END, v);
+            let in_bounds = _mm256_and_si256(lower_bound, upper_bound);
+            let mask = _mm256_movemask_epi8(in_bounds);
+            let offset = mask.trailing_ones();
+
+            if mask != 0xffff_ffff_u32 as i32 {
+                return p.add(offset as _).offset_from(range.start) as _;
+            }
+
+            p = p.add(VECTOR_SIZE);
+        }
+
+        while p < range.end {
+            if *p < b' ' || *p > b'~' {
+                return p.offset_from(range.start) as _;
+            }
+
+            p = p.add(1);
+        }
+    }
+
+    data.len()
+}
+
+#[no_mangle]
+#[inline(never)]
+pub fn to_ascii_or_hex_simd_v1(data: &[u8], out: &mut String) {
+    static mut BUFFER: [u8; 4] = *b"\\x00";
+    let mut tail = data;
+    while !tail.is_empty() {
+        let (ascii_piece, non_ascii_piece);
+
+        let i = find_ascii_simd(tail);
+        (non_ascii_piece, tail) = tail.split_at(i);
+        for c in non_ascii_piece {
+            out.push_str("\\x");
+            out.push(HEX_DIGIT[(c >> 4) as usize] as _);
+            out.push(HEX_DIGIT[(c & 0xf) as usize] as _);
+        }
+
+        if tail.is_empty() { return; }
+
+        let i = find_non_ascii_simd(tail);
+        (ascii_piece, tail) = tail.split_at(i);
+
+        for &c in ascii_piece {
+            if matches!(c, b'\\' | b'"') { out.push('\\'); }
+            out.push(c as _);
+        }
+        // let mut ascii_tail = ascii_piece;
+        // while !ascii_tail.is_empty() {
+        //     let i = find_backslash_or_double_quote_simd(ascii_tail);
+        //     let verbatim_piece;
+        //     (verbatim_piece, ascii_tail) = ascii_tail.split_at(i);
+
+        //     unsafe { out.push_str(std::str::from_utf8_unchecked(verbatim_piece)); }
+
+        //     if ascii_tail.is_empty() {
+        //         break;
+        //     }
+        //     out.push('\\');
+        //     let special_char;
+        //     (special_char, ascii_tail) = ascii_tail.split_at(1);
+        //     out.push(special_char[0] as _);
+        // }
+
+    }
+}
+
+#[no_mangle]
+#[inline(never)]
+pub fn to_ascii_or_hex_simd_v2(data: &[u8], out: &mut String) {
+    let dummy = String::new();
+    let mut buffer = std::mem::replace(out, dummy).into_bytes();
+    buffer.reserve_exact(buffer.capacity() - data.len() * 4);
+
+    let mut tail = data;
+    while !tail.is_empty() {
+        let (ascii_piece, non_ascii_piece);
+
+        let i = find_ascii_simd(tail);
+        (non_ascii_piece, tail) = tail.split_at(i);
+        for c in non_ascii_piece {
+            buffer.extend_from_slice(b"\\x");
+            buffer.push(HEX_DIGIT[(c >> 4) as usize]);
+            buffer.push(HEX_DIGIT[(c & 0xf) as usize]);
+            // out.push_str("\\x");
+            // out.push((c >> 4) as _);
+            // out.push((c & 0xf) as _);
+        }
+
+        if tail.is_empty() { break; }
+
+        let i = find_non_ascii_simd(tail);
+        (ascii_piece, tail) = tail.split_at(i);
+        for &c in ascii_piece {
+            if matches!(c, b'\\' | b'"') { buffer.push(b'\\'); }
+            buffer.push(c);
+        }
+    }
+
+    let s = unsafe { String::from_utf8_unchecked(buffer) };
+    std::mem::replace(out, s);
+}
+
+const HEX_DIGIT: [u8; 16] = *b"0123456789abcdef";
+
+#[no_mangle]
+#[inline(never)]
+pub fn to_ascii_or_hex(data: &[u8], out: &mut String) {
+    for &c in data {
+        if matches!(c, b' '..=b'~') {
+            if matches!(c, b'\\' | b'"') { out.push('\\'); }
+            out.push(c as _);
+        } else {
+            out.push_str("\\x");
+            out.push(HEX_DIGIT[(c >> 4) as usize] as _);
+            out.push(HEX_DIGIT[(c & 0xf) as usize] as _);
+        }
+    }
+}
+
+#[no_mangle]
+#[inline(never)]
+pub fn find_ascii_non_simd(data: &[u8]) -> usize {
+    let range = data.as_ptr_range();
+    let mut i = 0;
+
+    unsafe {
+        while i < data.len() {
+            let c = data[i];
+            if c >= b' ' && c <= b'~' {
+                return i;
+            }
+
+            i += 1;
+        }
+    }
+
+    return i;
+}
+
+#[no_mangle]
+#[inline(never)]
+pub fn find_non_ascii_non_simd(data: &[u8]) -> usize {
+    let range = data.as_ptr_range();
+    let mut i = 0;
+
+    unsafe {
+        while i < data.len() {
+            let c = data[i];
+            if c < b' ' || c > b'~' {
+                return i;
+            }
+
+            i += 1;
+        }
+    }
+
+    return i;
 }
 
 #[no_mangle]
--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,75 @@ unsafe fn perf_test(N: usize, L: usize) {
         data.extend_from_slice(as_slice(&n));
     }
     let get_slice = |i| &data[i * L..(i + 1) * L];
+
+    let mut results_ascii_or_hex_simd_v1 = Vec::with_capacity(N);
+    let mut results_ascii_or_hex_simd_v2 = Vec::with_capacity(N);
+    let mut results_ascii_or_hex_non_simd = Vec::with_capacity(N);
+    for i in 0..N {
+        let slice = get_slice(i);
+        results_ascii_or_hex_simd_v1.push(String::with_capacity(4 * slice.len()));
+        results_ascii_or_hex_simd_v2.push(String::with_capacity(4 * slice.len()));
+        results_ascii_or_hex_non_simd.push(String::with_capacity(4 * slice.len()));
+    }
+
+    for _ in 0..4 {
+
+        let t0 = std::time::Instant::now();
+        for i in 0..N {
+            let slice = get_slice(i);
+            to_ascii_or_hex(slice, &mut results_ascii_or_hex_non_simd[i]);
+        }
+        println!("non simd: {:?}", t0.elapsed());
+
+        let t0 = std::time::Instant::now();
+        for i in 0..N {
+            let slice = get_slice(i);
+            to_ascii_or_hex_simd_v1(slice, &mut results_ascii_or_hex_simd_v1[i]);
+        }
+        println!("simd_v1: {:?}", t0.elapsed());
+
+        let t0 = std::time::Instant::now();
+        for i in 0..N {
+            let slice = get_slice(i);
+            to_ascii_or_hex_simd_v2(slice, &mut results_ascii_or_hex_simd_v2[i]);
+        }
+        println!("simd_v2: {:?}", t0.elapsed());
+
+        for i in 0..N {
+            assert_eq!(results_ascii_or_hex_simd_v1[i], results_ascii_or_hex_non_simd[i], "{:?}", get_slice(i));
+            assert_eq!(results_ascii_or_hex_simd_v2[i], results_ascii_or_hex_non_simd[i], "{:?}", get_slice(i));
+
+            reset_string(&mut results_ascii_or_hex_simd_v1[i]);
+            reset_string(&mut results_ascii_or_hex_simd_v2[i]);
+            reset_string(&mut results_ascii_or_hex_non_simd[i]);
+        }
+    }
+}
+
+fn reset_string(s: &mut String) {
+    unsafe {
+        let mut v = std::mem::take(s).into_bytes();
+        v.set_len(0);
+        *s = String::from_utf8_unchecked(v);
+    }
+}
+
+unsafe fn perf_test_find(N: usize, L: usize) {
+    println!("============================================================");
+    println!("perf_test({N}, {L})");
+    let n_bytes = N * L;
+    let mut data: Vec<u8> = Vec::with_capacity(n_bytes);
+    for i in 0..n_bytes / 8 {
+        let n = rng::random_u64();
+        data.extend_from_slice(as_slice(&n));
+    }
+    let get_slice = |i| &data[i * L..(i + 1) * L];
     let mut simd_results = vec![0_usize; N];
     let mut non_simd_results = vec![0_usize; N];
     let mut non_simd_unrolled_results = vec![0_usize; N];
 
     for _ in 0..4 {
+
         let t0 = std::time::Instant::now();
         for i in 0..N {
             let slice = get_slice(i);
@@ -50,6 +114,26 @@ unsafe fn perf_test(N: usize, L: usize) {
             assert_eq!(simd_results[i], non_simd_results[i], "{:?}", get_slice(i));
             assert_eq!(non_simd_results[i], non_simd_unrolled_results[i], "{:?}", get_slice(i));
         }
+
+        let t0 = std::time::Instant::now();
+        for i in 0..N {
+            let slice = get_slice(i);
+            let index = find_non_ascii_simd(slice);
+            simd_results[i] = index;
+        }
+        println!("non-ascii simd: {:?}", t0.elapsed());
+
+        let t0 = std::time::Instant::now();
+        for i in 0..N {
+            let slice = get_slice(i);
+            let index = find_non_ascii_non_simd(slice);
+            non_simd_results[i] = index;
+        }
+        println!("non-ascii non simd: {:?}", t0.elapsed());
+
+        for i in 0..N {
+            assert_eq!(simd_results[i], non_simd_results[i], "{:?}", get_slice(i));
+        }
     }
 }
 
@@ -58,112 +142,6 @@ fn as_slice<T>(v: &T) -> &[u8] {
     unsafe { std::slice::from_raw_parts(v as *const T as *const u8, std::mem::size_of::<T>()) }
 }
 
-unsafe fn run() {
-    // dbg!(std::mem::size_of::<__m256i>());
-    // dbg!(std::mem::size_of::<__m512i>());
-    // dbg!(core::arch::x86_64::_mm256_cmpeq_epi8(1.into(), 1.into()));
-    // let all_bytes_zero = core::arch::x86_64::_mm256_setzero_si256();
-    // println!("{all_bytes_zero:?}");
-
-    println!("first ascii: ' ' {:02x}", b' ');
-    println!(" last ascii: '~' {:02x}", b'~');
-
-    let mut all_bytes = Vec::with_capacity(256);
-    for c in 0..256 {
-        all_bytes.push(c as u8);
-    }
-
-    let bytes_0_31 = _mm256_loadu_si256(all_bytes[0..32].as_ptr().cast());
-    println!("{}", inspect(&bytes_0_31));
-    let bytes_32_63 = _mm256_loadu_si256(all_bytes[32..64].as_ptr().cast());
-    println!("{}", inspect(&bytes_32_63));
-
-    let bytes_64_95 = _mm256_loadu_si256(all_bytes[64..96].as_ptr().cast());
-    println!("{}", inspect(&bytes_64_95));
-    let bytes_96_127 = _mm256_loadu_si256(all_bytes[96..128].as_ptr().cast());
-    println!("{}", inspect(&bytes_96_127));
-
-    let bytes_128_159 = _mm256_loadu_si256(all_bytes[128..160].as_ptr().cast());
-    println!("{}", inspect(&bytes_128_159));
-    let bytes_160_191 = _mm256_loadu_si256(all_bytes[160..192].as_ptr().cast());
-    println!("{}", inspect(&bytes_160_191));
-
-    let bytes_192_223 = _mm256_loadu_si256(all_bytes[192..224].as_ptr().cast());
-    println!("{}", inspect(&bytes_192_223));
-    let bytes_224_255 = _mm256_loadu_si256(all_bytes[224..256].as_ptr().cast());
-    println!("{}", inspect(&bytes_224_255));
-
-    let byte_ranges = [ bytes_0_31, bytes_32_63, bytes_64_95, bytes_96_127, bytes_128_159, bytes_160_191, bytes_192_223, bytes_224_255, ];
-
-    inspect!(ZERO);
-    inspect!(TOP_BIT);
-    inspect!(BITS_23);
-    inspect!(ASCII_END);
-
-    println!("================================================================================================");
-
-
-    // println!("------------------------------------------------------------------------------------------------");
-
-    // for bytes in byte_ranges {
-    //     let step1 = _mm256_andnot_si256(bytes, top_bit);
-    //     // No unsigned greater than???
-    //     let step1 = _mm256_cmpgt_epi8(ZERO, step1);
-    //     println!("{}", inspect(&step1));
-    // }
-
-    // println!("------------------------------------------------------------------------------------------------");
-
-    // for bytes in byte_ranges {
-    //     let step2 = _mm256_and_si256(bytes, BITS_23);
-    //     let step2 = _mm256_cmpgt_epi8(step2, ZERO);
-    //     println!("{}", inspect(&step2));
-    // }
-
-    // println!("------------------------------------------------------------------------------------------------");
-
-    // for bytes in byte_ranges {
-    //     let step3 = _mm256_cmpeq_epi8(bytes, ASCII_END);
-    //     let step3 = _mm256_cmpeq_epi8(step3, ZERO);
-    //     println!("{}", inspect(&step3));
-    // }
-
-    println!("================================================================================================");
-    println!("step 1 & 2 & 3");
-
-    println!("------------------------------------------------------------------------------------------------");
-
-    let mut masks: [__m256i; 8] = unsafe { std::mem::zeroed() };
-    for (&bytes, i) in byte_ranges.iter().zip(0..) {
-        let step1 = _mm256_andnot_si256(bytes, TOP_BIT);
-        let step1 = _mm256_cmpgt_epi8(ZERO, step1);
-
-        let step2 = _mm256_and_si256(bytes, BITS_23);
-        let step2 = _mm256_cmpgt_epi8(step2, ZERO);
-
-        let step3 = _mm256_cmpeq_epi8(bytes, ASCII_END);
-        let step3 = _mm256_cmpeq_epi8(step3, ZERO);
-
-        let step1_2 = _mm256_and_si256(step1, step2);
-        let step1_2_3 = _mm256_and_si256(step1_2, step3);
-
-        masks[i] = step1_2_3;
-
-        println!("{}", inspect(&step1_2_3));
-    }
-
-    for mask in masks {
-        let mask = _mm256_movemask_epi8(mask);
-        println!("{}", mask.leading_zeros());
-        // println!("{}", inspect(&mask));
-    }
-}
-
-// let zero = _mm256_set1_epi8(0b00000000 as _);
-// let top_bit = _mm256_set1_epi8(0b10000000 as _);
-// let bits_23 = _mm256_set1_epi8(0b01100000 as _);
-// let ascii_end = _mm256_set1_epi8(0x7f as _);
-
 fn check_find_ascii(data: &[u8]) {
     println!("------------------------------------------------------------------------------------------------");
     let i = find_ascii_simd(data);
@@ -171,12 +149,18 @@ fn check_find_ascii(data: &[u8]) {
     // for _ in 0..i { print!("    "); } println!("~~~~");
 }
 
+fn check_find_non_ascii(data: &[u8]) {
+    println!("------------------------------------------------------------------------------------------------");
+    let i = find_non_ascii_simd(data);
+    println!("{}", inspect_find_ascii_result(data, i));
+    // for _ in 0..i { print!("    "); } println!("~~~~");
+}
+
 fn main() {
-    // unsafe { run(); }
-    // check();
+    check();
     unsafe { perf_test(4096, 4096); }
     unsafe { perf_test(16 * 4096, 4096); }
-    unsafe { perf_test(16 * 4096, 8 * 4096); }
+    unsafe { perf_test(16 * 4096, 4 * 4096); }
     unsafe { perf_test(16 * 4096, 1024); }
 }
 
@@ -186,15 +170,19 @@ fn check() {
         all_bytes.push(c as u8);
     }
 
-    check_find_ascii(b"foo");
-    check_find_ascii(b"\x01\x02a");
-    check_find_ascii(b"\x0f\x10\x12a");
-    check_find_ascii(b"\x1e\x1f a");
-    check_find_ascii(&all_bytes);
-    check_find_ascii(&all_bytes[0x1e..]);
-    check_find_ascii(&all_bytes[0x1f..]);
-    check_find_ascii(&all_bytes[0x20..]);
-    check_find_ascii(&all_bytes[0x7d..]);
+    let mut s = String::with_capacity(256 * 4);
+    to_ascii_or_hex_simd_v1(&all_bytes, &mut s);
+    println!("{s}");
+
+    check_find_non_ascii(b"foo");
+    check_find_non_ascii(b"\x01\x02a");
+    check_find_non_ascii(b"\x0f\x10\x12a");
+    check_find_non_ascii(b"\x1e\x1f a");
+    check_find_non_ascii(&all_bytes);
+    check_find_non_ascii(&all_bytes[0x1e..]);
+    check_find_non_ascii(&all_bytes[0x1f..]);
+    check_find_non_ascii(&all_bytes[0x20..]);
+    check_find_non_ascii(&all_bytes[0x7d..]);
 }
 
 struct inspect_find_ascii_result<'a>(&'a [u8], usize);
@@ -203,7 +191,6 @@ impl std::fmt::Display for inspect_find_ascii_result<'_> {
         for (byte, i) in self.0.iter().copied().zip(0..) {
             if i == self.1 { f.write_str("\x1b[7m")?; }
             if matches!(byte, b' '..=b'~') {
-                f.write_str("   ")?;
                 write!(f, "{}", byte as char)?;
             } else {
                 f.write_str("\\x")?;
